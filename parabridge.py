3ad95d6eee31a642597213d86e403fa79c480a7f
Grigory Petrov <grigory.v.p@gmail.com>
2012-02-25 02:31:40 +0400

    Added start and stop functionality.

--- parabridge.py
+++ parabridge.py
@@ -5,13 +5,17 @@ import os
 import sys
 import argparse
 import subprocess
+import xmlrpclib
+
+COMM_PORT = 17963
 
 def start() :
   sFile = '{0}/parabridge_daemon.py'.format( sys.path[ 0 ] )
-  subprocess.Popen( [ 'python', sFile ] )
+  subprocess.Popen( [ 'python', sFile, str( COMM_PORT ) ] )
 
 def stop() :
-  print( "stop" )
+  oSrv = xmlrpclib.ServerProxy( 'http://localhost:{0}/'.format( COMM_PORT ) )
+  oSrv.stop()
 
 oParser = argparse.ArgumentParser( description = "Paradox to MySQL bridge." )
 oSubparsers = oParser.add_subparsers()
--- parabridge_daemon.py
+++ parabridge_daemon.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python
 # coding:utf-8 vi:et:ts=2
 
+import sys
+import argparse
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+
+g_fShutdown = False
+
+def stop() :
+  global g_fShutdown
+  g_fShutdown = True
+
+oParser = argparse.ArgumentParser( description = "Parabridge daemon" )
+oParser.add_argument( 'port', type = int, help = "Port to listen on" )
+oArgs = oParser.parse_args()
+oServer = SimpleXMLRPCServer( ( 'localhost', oArgs.port ), allow_none = True )
+oServer.register_function( stop )
+while not g_fShutdown :
+  oServer.handle_request()
+
