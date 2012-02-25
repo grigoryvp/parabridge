6c5b5156ce21b0d4009b71dbff8c8bcb28056ee7
Grigory Petrov <grigory.v.p@gmail.com>
2012-02-26 03:07:17 +0400

    Improved command-line switches help and added command to add task.

--- parabridge.py
+++ parabridge.py
@@ -11,6 +11,16 @@ import socket
 
 COMM_PORT = 17963
 COMM_ADDR = 'http://localhost:{0}/'.format( COMM_PORT )
+HELP_APP = """Paradox to SQLite bridge. This tool monitors specified
+  Paradox database and reflects all changes to specified SQLite database
+  that can be used by any application that has problems with Paradox."""
+HELP_START = """Starts background process that will monitor Paradox
+  databse."""
+HELP_STOP = """Stops background process that was previously started with
+  'start'."""
+HELP_TASK_ADD = """Adds task with specified name (name can be used later
+  to manage tasks), path to source Paradox database directory and path
+  to destination SQLite database file."""
 
 class Config( object ) :
 
@@ -40,20 +50,33 @@ class Config( object ) :
   def get( self, i_sName ) :
     return self.m_mItems[ i_sName ]
 
-def start() :
+def start( i_oArgs ) :
   sFile = '{0}/parabridge_daemon.py'.format( sys.path[ 0 ] )
   subprocess.Popen( [ 'python', sFile, str( COMM_PORT ) ] )
 
-def stop() :
+def stop( i_oArgs ) :
   try :
     oSrv = xmlrpclib.ServerProxy( COMM_ADDR )
     oSrv.stop()
   except socket.error :
     pass
 
-oParser = argparse.ArgumentParser( description = "Paradox to MySQL bridge." )
+def task_add( i_oArgs ) :
+  print( i_oArgs.task_name )
+  print( i_oArgs.task_src )
+  print( i_oArgs.task_dst )
+
+oParser = argparse.ArgumentParser( description = HELP_APP )
 oSubparsers = oParser.add_subparsers()
-oSubparsers.add_parser( 'start' ).set_defaults( handler = start )
-oSubparsers.add_parser( 'stop' ).set_defaults( handler = stop )
-oParser.parse_args().handler()
+oSubparser = oSubparsers.add_parser( 'start', help = HELP_START )
+oSubparser.set_defaults( handler = start )
+oSubparser = oSubparsers.add_parser( 'stop', help = HELP_STOP )
+oSubparser.set_defaults( handler = stop )
+oSubparser = oSubparsers.add_parser( 'task_add', help = HELP_TASK_ADD )
+oSubparser.set_defaults( handler = task_add )
+oSubparser.add_argument( 'task_name' )
+oSubparser.add_argument( 'task_src' )
+oSubparser.add_argument( 'task_dst' )
+oArgs = oParser.parse_args()
+oArgs.handler( oArgs )
 
