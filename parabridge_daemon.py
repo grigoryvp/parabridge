05c380c6e56b2878a4a4ad1fc6a3253db6c6a7cc
Grigory Petrov <grigory.v.p@gmail.com>
2012-02-27 01:09:06 +0400

    Source (Paradox) path is checked to be a directory.

--- parabridge_daemon.py
+++ parabridge_daemon.py
@@ -60,9 +60,11 @@ class Worker( threading.Thread ) :
     def setRes( i_sTxt ) :
       self.m_mResults[ i_sName ] = i_sTxt
     if not os.path.exists( i_sSrc ) :
-      return setRes( "Path \"{0}\" not found".format( i_sSrc ) )
+      return setRes( "Path \"{0}\" not found.".format( i_sSrc ) )
+    if not os.path.isdir( i_sSrc ) :
+      return setRes( "Path \"{0}\" is not a directory.".format( i_sSrc ) )
     sTime = time.strftime( '%Y.%m.%d %H:%M:%S' )
-    setRes( "Processed at {0}".format( sTime ) )
+    setRes( "Processed at {0}.".format( sTime ) )
 
   def shutdown( self ) :
     self.m_fShutdown = True
